9406a0c2b495b051062e9352d0aff334a4912e13
Kyle Wilcox <kyle.wilcox.unb@gmail.com>
2023-08-10 23:43:44 -0600

    Added Actor Hit trigger

---
+++ Source/Bango/Private/Puny/Default/Triggers/Trigger_ActorHit.cpp
@@ -0,0 +1,59 @@
+﻿#include "Puny/Default/Triggers/Trigger_ActorHit.h"
+
+#include "Puny/Core/InstigatorFilter.h"
+
+UPunyTrigger_ActorHit::UPunyTrigger_ActorHit()
+{
+	SignalOnHit = EPunyTriggerSignalType::None;
+}
+
+void UPunyTrigger_ActorHit::Enable_Implementation()
+{
+	if (SubscribedActor.IsValid())
+	{
+		if (SubscribedActor == TargetActor)
+		{
+			return;
+		}
+		else
+		{
+			Disable();
+		}
+	}
+
+	TargetActor->OnActorHit.AddDynamic(this, &ThisClass::OnActorHit);
+
+	SubscribedActor = TargetActor;
+}
+
+void UPunyTrigger_ActorHit::Disable_Implementation()
+{
+	if (!SubscribedActor.IsValid())
+	{
+		return;
+	}
+
+	SubscribedActor->OnActorHit.RemoveDynamic(this, &ThisClass::OnActorHit);
+
+	SubscribedActor = nullptr;
+}
+
+void UPunyTrigger_ActorHit::SetTargetActor(AActor* NewTargetActor)
+{
+	if (NewTargetActor == SubscribedActor)
+	{
+		return;
+	}
+
+	Disable();
+
+	TargetActor = NewTargetActor;
+
+	Enable();
+}
+
+void UPunyTrigger_ActorHit::OnActorHit(AActor* HitActor, AActor* InstigatorActor, FVector NormalImpulse, const FHitResult& Hit)
+{
+	SendSignal(FPunyTriggerSignal(SignalOnHit, InstigatorActor));
+}
+
